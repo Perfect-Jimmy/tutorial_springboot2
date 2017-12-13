4f215fa9fa14dbc4902f15aee5fe3d0639e28e54
jimmy <773792867@qq.com>
2017-12-13 18:18:43 +0800

    Initial commit

---
+++ tutorial_perfect/src/main/java/com/tutorial/App.java
@@ -0,0 +1,14 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jimmy on 2017/12/13.
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
